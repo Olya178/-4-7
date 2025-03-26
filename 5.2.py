98ac1dd411e822f20fe5b2a3c602f8022e201dd5
Olya178 <sumakovaolga53@gmail.com>
2025-03-26 15:06:39 +0300

    лаба 4-7

--- 5.2.py
+++ 5.2.py
@@ -1,7 +1,7 @@
 words = []
 while True:
     word = input("Введите слово (или 'stop' для завершения): ")
-    if word.lower() == "stop":  # проверка, ввел ли слово стоп или нет
+    if word.lower() == "stop":
         break
     words.append(word)
 
--- 6.1.py
+++ 6.1.py
@@ -4,3 +4,30 @@ def de(number):
     else:
         return "Не делится"
 print(de(int(input("Введите число:"))))
+
+
+def user(num):
+    try:
+        result = 100 / num
+        return f"Результат: {result}"
+    except ValueError:
+        return "Ошибка: Введите корректное число!"  # ошибка ввод строки вместо числа
+    except ZeroDivisionError:
+        return "Ошибка: Деление на ноль невозможно!" # деление на 0
+    except Exception as e:
+        return f"Ошибка: {e}"  #  другие исключения
+print (user(int(input("Введите число для деления 100: "))))
+
+
+def mag(dat):
+    dat=dat.split(".")
+    return int(dat[0]) * int(dat[1]) == int(dat[2][-2:])
+print(mag(input("Введите дату :")))
+
+
+def num(d):
+    d = [int(i) for i in d]
+    print(d[:len(d)//2])
+    print(d[len(d) // 2:])
+    return sum(d[:len(d)//2]) == sum(d[len(d)//2:])
+print(num(input("Введите дату :")))
