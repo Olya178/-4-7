7fed646e5eacc27c6f21e7521618dfd837c5930d
Olya178 <sumakovaolga53@gmail.com>
2025-03-26 11:55:19 +0300

    лаба 4-7

--- лаба 4.3.py
+++ лаба 4.3.py
@@ -1,6 +1,6 @@
 def year(y):
     if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
-        return "Год", y, " — високосный"
+        return f"Год {y} — високосный"
     else:
         return "Это год не високосный"
 print(year(int(input("Введите номер года:"))))
