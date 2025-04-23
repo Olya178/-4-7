3c8ad6f3dd96bb27b9bb1d96caea8b82900b119e
Olya178 <sumakovaolga53@gmail.com>
2025-04-23 14:46:02 +0300

    лаба 9

--- 9.4.py
+++ 9.4.py
@@ -1,31 +1,34 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-folder = "D:\\лабы питон\\pythonProject\\лаба 9"
-watermark_text = "АИП"
+orig_dir = "D:\\лабы питон\\pythonProject\\лаба 9"
+watermark_text = "water"
 
-font = ImageFont.load_default()
+font_path = "C:\\Windows\\Fonts\\arial.ttf"
+font = ImageFont.truetype(font_path, size=50)
 
-for file in os.listdir(folder):
-    if file.endswith(".jpg"):
-        path = os.path.join(folder, file)
+for file in os.listdir(orig_dir):
+    if file.endswith(".jpg"): #проверяет на что оканчивается документ
+        path = os.path.join(orig_dir, file)
 
         image = Image.open(path).convert("RGBA")
 
         txt_layer = Image.new("RGBA", image.size, (255, 255, 255, 0))
         draw = ImageDraw.Draw(txt_layer)
 
-        text_w, text_h = draw.textsize(watermark_text, font)
-        x = (image.width - text_w) // 2
-        y = (image.height - text_h) // 2
+        bbox = draw.textbbox((0, 0), watermark_text, font=font)
+        text_w = bbox[2] - bbox[0] #координаты правой границы вычитают координаты левой границы
+        text_h = bbox[3] - bbox[1] #координаты нижней границы вычитают координаты верхней границы
+
+        x = (image.width - text_w)//2
+        y = (image.height - text_h)//2
 
         draw.text((x, y), watermark_text, font=font, fill=(255, 255, 255, 255))
 
+        watermarked = Image.alpha_composite(image, txt_layer)#объединение текста и знака
 
-        watermarked = Image.alpha_composite(image, txt_layer)        # объединяем изображение и текстовый слой
-
-        # Сохраняем как PNG
-        new_file = os.path.join(folder, "wm_" + os.path.splitext(file)[0] + ".png")
+        new_file = os.path.join(orig_dir, "wm_" + os.path.splitext(file)[0] + ".png")
         watermarked.save(new_file)
 
-print("Готово! Водяные знаки добавлены в центр (без прозрачности).")
+print("Водяные знаки добавлены")
+
