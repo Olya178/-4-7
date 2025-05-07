800b39ff5890fa73870c2ecffa63557ce5a52a43
Olya178 <sumakovaolga53@gmail.com>
2025-05-07 14:00:51 +0300

    лаба 12

--- 12.3.py
+++ 12.3.py
@@ -1,4 +1,3 @@
-import json
 with open("D:\\лабы питон\\pythonProject\\en-ru.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
 
@@ -7,8 +6,8 @@ ru_en = {}
 for line in lines:
     if '-' not in line:
         continue
-    en, ru = line.strip().split('-', 1)       # pазделяем английское и русское
-    en = en.strip()
+    en, ru = line.strip().split('-', 1)
+    en = en.strip() #убираем пробелы переводы строк с начала и конца строки
     rus_words = [w.strip() for w in ru.split(',')]
 
     for rw in rus_words:
@@ -16,9 +15,8 @@ for line in lines:
             ru_en[rw] = []
         ru_en[rw].append(en)
 
-
 with open("ru-en.txt", "w", encoding="utf-8") as f:
     for ru_word in sorted(ru_en):
-        translations = sorted(set(ru_en[ru_word]))  # Убираем повторы и сортируем
+        translations = sorted(set(ru_en[ru_word]))  # убираем повторы и сортируем
         f.write(f"{ru_word} – {', '.join(translations)}\n")
 print("Файл сохранен")
